9d7468e9394d35146d50b959eac9ba839b22be31
AbhishekG009 <abhishekgawas774@gmail.com>
2026-01-01 19:14:22 +0530

    Pallindromeof string

---
+++ src/main/java/org/example/NORMAL_CODING/StringOrNumberisPallindromeOrNot.java
@@ -0,0 +1,24 @@
+package org.example.NORMAL_CODING;
+
+public class StringOrNumberisPallindromeOrNot {
+    public static  boolean isPallindrome(String input)
+    {
+        int left =0;
+        int right = input.length()-1;
+        while(left < right)
+        {
+            if(input.charAt(left) != input.charAt(right))
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPallindrome("MADAM"));
+    }
+
+}
